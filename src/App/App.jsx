0e70256950629b3288f4e09170b6ff3928dea1eb
Mike24 <59624793+hayk24@users.noreply.github.com>
2020-01-15 13:05:54 -0800

    First project

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,22 @@
+// React Librery
 import React from 'react';
- 
+// App component stayles
+import './App.scss'; 
+// my components
+import Header from './Header/Header.jsx';
+import Nav from './Nav/Nav.jsx';
+import Main from './Main/Main.jsx';
+import Footer from './Footer/Footer.jsx';
+
 const App = () => {
     
-    return(
-        <div>Hayk's THE APP</div>
+    return (
+        <div className='App'> 
+            <Header />
+            <Nav />
+            <Main />
+            <Footer />
+        </div>
     )
 }
 
---
+++ src/App/Header/Header.jsx
@@ -0,0 +1,12 @@
+// Node Packages not need realtive paths
+import React from 'react';
+import './Header.scss';
+
+// this is may commponents
+const Header = () => {
+    return (
+        <header className={'Header'}>My Component Header</header>
+    );
+}
+// every componoyts must be exported
+export default Header;
